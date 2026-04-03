6fecb53bd226f14fc3086d15ac051745ae9ef137
egamasa <40469431+egamasa@users.noreply.github.com>
2026-04-03 12:47:28 +0900

    remove unused files

--- powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-﻿# $PROFILE
-
-# Vim
-# Japanese: https://www.kaoriya.net/software/vim/
-# Original: https://www.vim.org/download.php
-set-alias vi 'C:\vim82ja-x64\vim.exe'
-set-alias vim 'C:\vim82ja-x64\vim.exe'
